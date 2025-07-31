f5eda14a0c1d97798c6a75d166b4b9c764f45ec0
QaishGit <160156299+QaishGit@users.noreply.github.com>
2025-07-31 11:33:37 +0530

    Learning Backend

---
+++ Lecture 2 - first Node code/first.js
@@ -0,0 +1,8 @@
+console.log('Starting Backend');
+
+const fs = require('fs');
+
+fs.writeFile('output.txt', 'Writing file', (err) => {
+  if(err) console.log('Error Occured');
+  else console.log('File written successfully');
+});
