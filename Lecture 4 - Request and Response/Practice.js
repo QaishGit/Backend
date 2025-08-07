9b635ee98145a3589955bb8f052f9a119fb0c279
QaishGit <160156299+QaishGit@users.noreply.github.com>
2025-08-07 10:50:50 +0530

    Practice

---
+++ Lecture 4 - Request and Response/Practice.js
@@ -0,0 +1,45 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  console.log(req.url, req.method, req.headers);
+
+  if(req.url === '/'){
+    res.setHeader('Context-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>Practice</title></head>');
+    res.write(`<body><a href='/men'>Men</a><br>`);
+    res.write(`<a href='/women'>Women</a><br>`);
+    res.write(`<a href='/kids'>Kids</a></body>`);
+    res.write('</html>');
+    return res.end();
+  }
+  else if(req.url.toLowerCase === '/men'){
+    res.setHeader('Context-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>Practice</title></head>');
+    res.write(`<body><h1>Welcome to Men's Section</h1></body>`);
+    res.write('</html>');
+    return res.end();
+  }
+  else if(req.url.toLowerCase === '/women'){
+    res.setHeader('Context-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>Practice</title></head>');
+    res.write(`<body><h1>Welcome to Women's Section</h1></body>`);
+    res.write('</html>');
+    return res.end();
+  }
+  res.setHeader('Context-Type', 'text/html');
+  res.write('<html>');
+  res.write('<head><title>Practice</title></head>');
+  res.write(`<body><h1>Welcome to Men's Section</h1></body>`);
+  res.write('</html>');
+  res.end();
+
+});
+
+const PORT = 3000;
+
+server.listen(PORT, () => {
+  console.log(`Server is running on address https://localhost:${PORT}`);
+});
