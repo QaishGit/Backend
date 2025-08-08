872fc3a91c148d4d478622bfcb22e3baddb36c77
QaishGit <160156299+QaishGit@users.noreply.github.com>
2025-08-08 11:54:22 +0530

    Parsing requests

---
+++ Lecture 5 - Parsing Rquest/Parsing request.js
@@ -0,0 +1,67 @@
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+  console.log(req.url, req.method);
+
+  if(req.url === '/'){
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>Learning Backend</title></head>');
+    res.write('<body><h1>Fill the details</h1></body>');
+    res.write('<form action="/submitted" method="POST">');
+    res.write('<input type="text" name="username" placeholder="Enter your name"><br>');
+    res.write('<label for="male">Male</label>');
+    res.write('<input type="radio" id="male" name="gender" value="male">');
+    res.write('<label for="female">Female</label>');
+    res.write('<input type="radio" id="female" name="gender" value="female"><br>');
+    res.write('<input type="submit" value="Submit">');
+    res.write('</form>');
+    res.write('</body>');
+    res.write('</html>');
+    return res.end();
+  }
+  else if(req.url.toLowerCase() === '/submitted' && req.method === "POST"){
+
+    //Reading chunks and converting into information
+    const body = [];
+
+    req.on('data', chunk => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+
+    req.on('end', () => {
+      const fullBody = Buffer.concat(body).toString();
+      console.log(fullBody);
+
+      //Getting data in original form
+      const params = new URLSearchParams(fullBody);
+
+      // const bodyObject = {};
+      // for(const[key, val] of params.entries()){
+      //   bodyObject[key] = val;
+      // }
+
+      const bodyObject = Object.fromEntries(params);
+      console.log(bodyObject);
+    });
+
+    fs.writeFileSync('user.txt', 'Cash');
+    res.statusCode = 302;
+    res.setHeader('Location', '/');
+    return res.end();
+  }
+
+  res.setHeader('Content-Type', 'text/html');
+  res.write('<html>');
+  res.write('<head><title>Learning Backend</title></head>');
+  res.write(`<body><h1>Form Submitted</h1></body>`);
+  res.write('</html>');
+  res.end();
+});
+
+const PORT = 3000;
+server.listen(PORT, () => {
+  console.log(`Hi! Server running on address http://localhost:${PORT}`);
+});
